f3b6d4f6168c3e1e093ef1f396cf8ddb10c08b60
Saima Fathima <saima.fathi2011@gmail.com>
2024-11-26 18:57:17 +0530

    Add files via upload

---
+++ pro1.py
@@ -0,0 +1,12 @@
+print("Birthday of 5 friends\n")
+#storing birthdays in variables and printing
+a="12/03/2012"
+print("Aanya-",a)
+b="23/05/2013"
+print("Alba-",b)
+c="17/07/2014"
+print("Sanaya-",c)
+d="19/08/2011"
+print("Saima-",d)
+e="27/11/2015"
+print("Souza-",e)
